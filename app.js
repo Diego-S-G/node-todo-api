104e15fbf7bac794f5227345b26961f73e500168
Diego-S-G <diego07goncalves@gmail.com>
2024-12-23 09:54:05 -0300

    Node.js REST API Continued.

--- app.js
+++ app.js
@@ -3,36 +3,16 @@ const app = express();
 const port = 3000;
 const router = express.Router();
 
+const Todo = require('./todo');
+
 router.get('/', (req, res) => res.send('Hello World!'));
 
 router.route('/todos')
     .get((req, res) => {
-        res.json([{
-            "_id": 1,
-            "text": 'Do the laundry',
-            "done": false
-        }, {
-            "_id": 2,
-            "text": 'Do the dishes',
-            "done": true
-        },
-        {
-            "_id": 3,
-            "text": 'Do the homework',
-            "done": true
-        },
-        {
-            "_id": 4,
-            "text": 'Do the shopping',
-            "done": false
-        },
-        {
-            "_id": 5,
-            "text": 'Do the cooking',
-            "done": false
-        }
-        ]);
-    })
+        Todo.all(function(data) {
+            res.json(data);
+        });
+    });
 
 app.use('/', router);
 
---
+++ todo.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+mongoose.set('useFindAndModify', false);
+
+mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true });
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function() {
+    console.log('Connected to MongoDb!!');
+});
+
+const todoSchema = new mongoose.Schema({
+    text: String,
+    done: Boolean
+});
+
+todoSchema.statics.all = function(callback) {
+    return Todo.find({}, function(err, todos) {
+        callback({todos: todos});
+    });
+};
+const Todo = mongoose.model('Todo', todoSchema);
+
+module.exports = Todo;
