4390824edc3a39fc4c0e5d5892f9a193a22d9a67
Diego-S-G <diego07goncalves@gmail.com>
2024-12-23 13:54:34 -0300

    Serve the React App from the Node.js App (pega o js, html e css do react e joga no public do node, fazendo alterações de acordo como url nos fetchs).

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const port = 3000;
 const router = express.Router();
 const bodyParser = require('body-parser');
 
-app.use(express.static('public'));
-
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "PUT, POST, GET, OPTIONS");
     next(); // Allow the request to proceed to the next middleware or route handler
 }); // Allow cross-origin requests - CORS
+
+app.use(express.static('public'));
 
 app.use(bodyParser.json());
 
