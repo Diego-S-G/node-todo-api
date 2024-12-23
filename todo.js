d67732f1770e9c8a0f8b1060907067e20f80d694
Diego-S-G <diego07goncalves@gmail.com>
2024-12-23 14:11:09 -0300

    Prepara ambiente caso fosse usar para produção no heroku.

--- todo.js
+++ todo.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true });
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017';
+mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 
